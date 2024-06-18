1f318a703cadccc0dee9b1996177394b0b0b763a
IgnacioGurrea <gurreaignasi@gmail.com>
2024-06-18 22:39:44 +0200

    header and subheader added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { CardComponent } from './card/card.component';
 import { DetailsComponent } from './details/details.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './head-foot/header/header.component';
+import { FooterComponent } from './head-foot/footer/footer.component';
+import { SubHeaderComponent } from './head-foot/sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,10 @@ import { SharedModule } from './shared/shared.module';
     HomeComponent,
     CardComponent,
     DetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    SubHeaderComponent,
+    
   ],
   imports: [
     SharedModule,
