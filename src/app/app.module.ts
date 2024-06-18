7760ae0e58b1eceac18c74031ebb3e0f12d203e4
IgnacioGurrea <gurreaignasi@gmail.com>
2024-06-18 19:30:40 +0200

    shared module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
 import { DetailsComponent } from './details/details.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    DetailsComponent
+    DetailsComponent,
   ],
   imports: [
+    SharedModule,
     AppRoutingModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
